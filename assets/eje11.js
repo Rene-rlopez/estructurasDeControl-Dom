f2e071dbe4534d2a72e17cccb4ca40c8d248a6b3
Rene Lopez <132025382+Rene-rlopez@users.noreply.github.com>
2023-07-09 00:21:35 -0600

    bootstrap añadido a los ejercicios 3 y 7, autores añadidos al head de los ejercicios actuales y ejercicio 11 completo

--- assets/eje11.js
+++ assets/eje11.js
@@ -5,3 +5,24 @@
 //          Si ºF está entre 32 y 68, sale la frase “Temperatura adecuada”
 //          Si ºF está entre 68 y 96, sale la frase “Temperatura alta”
 //          Si no está entre ningún caso anterior la frase “Temperatura desconocida”
+
+// Formula para convertir grados Celsius a Fahrenheit: (0 °C × 9/5) + 32 = 32 °F
+
+
+function convertirTemperatura() {
+    let temperaturaCelsius = parseFloat(document.getElementById("celsius").value);
+    let temperaturaFahrenheit = (temperaturaCelsius * 9/5) + 32;
+    let resultadoElement = document.getElementById("resultado");
+
+    resultadoElement.innerHTML = "Temperatura en Fahrenheit: " + temperaturaFahrenheit.toFixed(2);
+
+    if (temperaturaFahrenheit >= 14 && temperaturaFahrenheit < 32) {
+        resultadoElement.innerHTML += "<br>Temperatura baja";
+    } else if (temperaturaFahrenheit >= 32 && temperaturaFahrenheit < 68) {
+        resultadoElement.innerHTML += "<br>Temperatura adecuada";
+    } else if (temperaturaFahrenheit >= 68 && temperaturaFahrenheit < 96) {
+        resultadoElement.innerHTML += "<br>Temperatura alta";
+    } else {
+        resultadoElement.innerHTML += "<br>Temperatura desconocida";
+    }
+}
