1bf41ffebea8b0dc5a3836290545dd01a2abb2e3
Cristian Monroy <monroytupai@gmail.com>
2023-07-13 00:13:18 -0600

    Ejercicio6 actualizado

--- assets/Ejercicio6.js
+++ assets/Ejercicio6.js
@@ -1,22 +1,42 @@
+function calcularDescuento() {
+    let Costa = "Costa";
+    let Panchimalco = "Panchimalco";
+    let Puerto = "Puerto";
+    let pCosta = 50;
+    let pPanchimalco = 100;
+    let pPuerto = 125;
+    let descuentoCosta = 0.05;
+    let descuentoPanchi = 0.1;
+    let descuentoPuerto = 0.15;
+    let descuento = 0;
 
-function calcularDescuento(destino) {
-    var Costa = "La costa del sol";
-    var Panchimalco = "Panchimalco";
-    var Puerto = "Puerto el triunfo";
-    var descuento = 0;
+    totalC = descuentoCosta * pCosta;
+    totalCosta = pCosta - totalC;
 
-    if (destino === Costa) {
+    totalP = descuentoPanchi * pPanchimalco;
+    totalPanchimalco = pPanchimalco - totalP;
+
+    totalpuerto = descuentoPuerto * pPuerto;
+    totalPuertoo = pPuerto - totalpuerto;
+    let selectedDestination = event.target.id;
+
+    if (selectedDestination === Costa) {
         descuento = 0.05;
-    } else if (destino === Panchimalco) {
+    } else if (selectedDestination === Panchimalco) {
         descuento = 0.1;
-    } else if (destino === Puerto) {
+    } else if (selectedDestination === Puerto) {
         descuento = 0.15;
     }
 
-    var resultadoElement = document.getElementById('resultado');
-    if (descuento > 0) {
-        resultadoElement.innerHTML = "<br><div class='alert alert-success' role='alert'><strong>Se aplicará un descuento del " + (descuento * 100) + "% en el viaje</strong></div>";
-    } else {
+    let resultadoElement = document.getElementById('resultado');
+    if (selectedDestination === Costa) {
+        resultadoElement.innerHTML = "<br><div class='alert alert-success' role='alert'><strong>Se aplicará un descuento del " + (descuento * 100) + "% en el viaje del costo, su total sera $"+ totalCosta+"</strong></div>";
+    } else if (selectedDestination === Panchimalco){
+        resultadoElement.innerHTML = "<br><div class='alert alert-success' role='alert'><strong>Se aplicará un descuento del " + (descuento * 100) + "% en el viaje del costo, su total sera $"+ totalPanchimalco+"</strong></div>";
+    }else if (selectedDestination === Puerto){
+        resultadoElement.innerHTML = "<br><div class='alert alert-success' role='alert'><strong>Se aplicará un descuento del " + (descuento * 100) + "% en el viaje del costo, su total sera $"+ totalPuertoo+"</strong></div>";
+    } 
+    else {
         resultadoElement.innerHTML = "<div class='alert alert-danger' role='alert'><strong>No se aplica descuento en el viaje</strong></div>";
     }
 }
